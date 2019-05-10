6078dcf11a3ac967e56f32e52b1cfaddea28862c
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-10 13:21:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  h = array.length
+  index = 0
+  while h > 0
+    yield array[index]
+    index +=1
+    h -= 1
+  end
+  array
 end
